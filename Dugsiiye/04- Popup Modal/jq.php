63713baecd5afa56251ec973a7ada4cccd6a3bdb
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-10 13:45:41 -0800

    Add files via upload

---
+++ Dugsiiye/04- Popup Modal/jq.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jquery</title>
+</head>
+
+<body>
+    <h1 class="Heading_1">hello world</h1>
+    <h2 id="Heading_1">hello world</h2>
+    <h3>hello world</h3>
+
+    <button> Click here</button>
+
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="jq.js"></script>
+
+</body>
+
+</html>

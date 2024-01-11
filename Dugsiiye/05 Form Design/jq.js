6ad9345c29fcb1640b0314be29e37c3b5fe58a6e
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-10 21:18:36 -0800

    Add files via upload

---
+++ Dugsiiye/05 Form Design/jq.js
@@ -0,0 +1,13 @@
+$(".Heading_1").html("this is HTML js Document");
+$("#Heading_1").css("backgroundColor", "#774585");
+$("h3").css("fontSize", "50px");
+
+
+// Events in JQuery
+
+// $("button").on("click", function() {
+//     $("body").css("backgroundColor", "#112410");
+// })
+$("button").click(function() {
+    $("body").css("backgroundColor", "#112410");
+})
---
+++ Dugsiiye/06  API/api.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Content-type: application/json");
+
+// function Read All
+
+
+
+
+
+
+
+?>
